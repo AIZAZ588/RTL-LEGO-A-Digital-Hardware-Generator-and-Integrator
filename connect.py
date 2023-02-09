5c965db72b254b12431c00829869e96e7200798d
aizaz <aizazkhan022017@gmail.com>
2023-02-09 17:46:40 +0500

    updated to new files

--- connect.py
+++ connect.py
@@ -1,132 +1,86 @@
-#!/usr/bin/env python3
-from Baseboard.key_val import file_name,folder_name
-import fileinput
+#/usr/bin/env python3
 import os
 import argparse
+import json
+import Extracting_data
+
+os.chdir('..\Baseboard')
+with open("key_val_file.json", "r")as f:
+    content = json.load(f)
+    for i in content:
+        fileName = content['toplevelfile']['file_name']
+        folder_name = content['toplevelfile']['folder_name']
+        child_path = content['toplevelfile']['child_path']
+
+
+os.chdir('..\library')
+
 parser = argparse.ArgumentParser()
-find = True
-ii = False #--> input instance 
-oi = False #--> output instance
-ip = False #--> input port
-op = False #--> output port
-instance_found= False
-port_found = False
-input_found = False
-i=0
-no_of_inputs = 0
-no_of_outputs = 0
-input_instance = False
-output_instance = False
-###########################################
-def listToString(s):
-   
-    # initialize an empty string
-    str1 = " "
-    
-    # return string 
-    return (str1.join(s))
-###########################################
+parser.add_argument('-i', '--instance_name',
+                    default='inst_uut', help='Name of instance')
+parser.add_argument('-f', '--file_name',
+                    help='Name of file from which instance is taken', required=True)
+args = parser.parse_args()
+file = args.file_name
+instance = args.instance_name
 
-##################################################################################
-parser = argparse.ArgumentParser(
-    description= "usage: -ip or --inputport,-i or --inputinstace,-o or --outputinstance ,-op or --outputport, "
-)
-parser.add_argument(
-    '-ip','--inputport',action='append',nargs='+' ,help="usage : -ip or --port",type=str
-)
-parser.add_argument(
-    '-i','--inputinstace',action='append',nargs='+' ,help="usage : -i or --inputinstace",type=str
-)
-parser.add_argument(
-    '-op','--outputport',action='append',nargs='+' ,help="usage : -op or --input",type=str
-)
-parser.add_argument(
-    '-o','--outputinstance',action='append',nargs='+' ,help="usage : -o or --outputinstance",type=str
-)
-arg = parser.parse_args()
-if arg.inputinstace:
-    input_instance = arg.inputinstace
-else:
-    print('Enter input instance name -i,--inputinstace')
-    exit()
-if arg.inputport:
-    no_of_inputs = len(arg.inputport)
-else:
-    print("Enter at least one input port")
-    exit()
-if arg.outputinstance:
-    output_instance = arg.outputinstance
-else:
-    print("Enter output instance name")
-    exit()
-if arg.outputport:
-    no_of_outputs = len(arg.outputport)
-else:
-    print("Enter at least one output port")
-    exit()
-if no_of_inputs < no_of_outputs or no_of_inputs > no_of_outputs:
-    print("no of input ports and output ports are unqual!")
-    exit()
-input_port = arg.inputport 
-output_port = arg.outputport  
 
-###########################################################################################
-for No_inputs in input_port:
-    for input_p in input_port:
-        input_p  = listToString(No_inputs).replace(' ','')
-    for output_p in output_port[i]:
-        output_p = listToString(output_p).replace(' ','')
-    for  input_inst in  input_instance:
-        input_inst = listToString(input_inst)
-    for output_inst in output_instance:
-        output_inst = listToString(output_inst)
-    print(end='\n')
-    print('input instance :',input_inst,'--> port : ',input_p,end='\n')
-    print('output instance :',output_inst,'--> port :',output_p,end='\n\n')
+def extract_data(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    module_name = "module_name"
+    in_module = False
+    input_or_output_count = 0
+    output_string = ""
+    for line in lines:
+        if 'module' in line and not in_module:
+            in_module = True
+            module_name = line.split()[1]
+            output_string += module_name + ' ' + f'{instance}' + '\n'
+            output_string += "(\n"
+        if 'input' in line or 'output' in line:
+            input_or_output_count += 1
+            words = line.strip().split()
+            x = words[-1]
+            if "," in x:
+                x = x.split(",")[0]
+            if input_or_output_count == sum(('input' in line) or ('output' in line) for line in lines):
+                output_string += '.' + x + '\t\t\t()\n'
+            else:
+                output_string += '.' + x + '\t\t\t(),\n'
+    os.chdir('..')
+    os.chdir('Baseboard')
+    with open(f"{fileName}", "r") as f:
+        content = f.read()
+        lines = content.split('\n')
+        for i, line in enumerate(lines):
+            if instance in line:
+                print(
+                    f'instance {instance} already exists at line {i+1}. Please Enter another name ')
+                exit()
+        with open(f"{fileName}", "a+") as f:
+            if 'endmodule' in content:
+                r_end = (f.tell())-9
+                x = f.truncate(r_end)
+                f.write('\n\n' + output_string)
+                f.write(');')
+                f.write('\n\nendmodule')
 
-    os.chdir(folder_name)
-    with open("key_val.py",'r+') as file:
-        while find:
-            find =  file.readline()     
-            if input_inst in find:
-                input_ist_list = find
-                ii = True
-                #print(input_ist_list) #--> input instance list of ports
-                if input_ist_list.__contains__(input_p): 
-                    ip = True 
-            if output_inst in find:
-                output_inst_list = find
-                oi = True
-                #print(output_inst_list) #--> output instance list of ports
-                if output_inst_list.__contains__(output_p): 
-                    op = True 
-                    connected_ports = f"\n{input_inst} = {{'{input_p}':'{output_p}'}}"
-                    with open('key_val.py','r+') as file:
-                        find = file.read()
-                        # if connected_ports in find:
-                        #     connected_ports = f"\n{input_inst}['{input_p}]'='{output_p}']"
-                        #     file.write(connected_ports)
-                        # else:
-                        file.write(connected_ports)
-                    for line in fileinput.FileInput(file_name,inplace=1):
-                        line = line.replace('\n','')
-                        if input_inst in line:
-                            input_found = True
-                        if  input_found and f'.{input_p}' in line:
-                            line = line.replace(line,f'.{input_p}\t\t\t\t\t({output_p}),')
-                            input_found = False
-                        print(line)
-                    print('connected')
-                    oi = True
-                    os.chdir('..')
-                    i+=1
-                    break
-                    
-    if ii == False:
-        print('input instance not found!')
-    if oi == False:
-        print('output instance not found!')
-    if ip == False:
-        print('input ports not found!')
-    if op == False:
-        print('output ports not found!')
+
+extract_data(file)
+
+os.chdir('..')
+os.chdir('library')
+data = Extracting_data.get_ranges_from_file(file)
+
+os.chdir('..')
+os.chdir('Baseboard')
+with open("key_val_file.json", "r") as f:
+    content = f.read()
+with open('key_val_file.json', 'a+') as f:
+    if "}" in content:
+        r_end = (f.tell())-3
+        x = f.truncate(r_end)
+    f.write(f',\n\"{instance}\":')
+    json.dump(data, f, indent=4)
+    f.write("\n}")
--- plug.py
+++ plug.py
@@ -1,155 +1,86 @@
-#!/usr/bin/env python3
-#########################  Imports ###############################################################
-from Baseboard.key_val import file_name,folder_name,child_path
-import argparse
+#/usr/bin/env python3
 import os
-found_module = False
-#print(list_modules.modules)
-print("Top level file is  : ",file_name)
-no_of_inputs = 0
-no_of_outputs = 0
-input_list = []
-output_list = []
-library = r"..\LAGO\library"
-###################### Parser block ############################################################
-    
-parser = argparse.ArgumentParser(
-    description= " use -m or --module for module name"
-)
-parser.add_argument(
-    '-m','--module',action='append',nargs='+' ,help="usage : -m or --module",type=str
-)
-parser.add_argument(
-    '-n','--name',action='append',nargs='+' ,help="usage : -n or --name to name your instane",type=str
-)
-arg = parser.parse_args()
-no_of_name = 0
-no_of_modules = 0
-if arg.module:
-    no_of_modules  = len(arg.module) 
-else:
-    print("Enter atleast one module name!")
-    exit()
-if arg.name:
-    no_of_name = len(arg.name) 
-#for args in vars(arg):
-    #print(args, getattr(arg, args))
-###########################################
-def listToString(s):
-   
-    # initialize an empty string
-    str1 = " "
-    
-    # return string 
-    return (str1.join(s))
-###########################################
-os.chdir('library')
-library_list = os.listdir()
-os.chdir('..')
-#print(library_list)
-###########################################
-i=0
-for Mname in arg.module:
-    module_name = listToString(Mname)
-    inst_name = listToString(Mname).replace(".sv","")
-    if i<no_of_name:
-            name = arg.name[i]
-            new_name = listToString(name)
-            inst_name = new_name
-            i+=1
-            print('new name of module is :',inst_name)
-            
-    #'''
-############################# Reading Base file #################################################
-    #os.chdir('..')
-    os.chdir(folder_name)
-    with open(rf"{file_name}", 'r+') as look_file:
-        look = look_file.read()
-        count = look.count(inst_name)
-        if count:
-            inst_name = f"{inst_name}_{count+1}"
-        t_inst_count = look.count("inst")
-        print("instnace  : ",t_inst_count+1)
-        
-    ######################### file handling block ###############################################
-    for look in library_list:
-        if module_name in library_list:
-            found_module = True
-            print(f"\n{module_name} found!") 
-            if found_module:
-                find=True
-                inp  = False
-                out  = False
-                os.chdir('..')
-                os.chdir('library')
-                with open(rf"{module_name}",'r+') as module:  # opening module(counter etc)   
-                    os.chdir('..')
-                    os.chdir(folder_name)
-                    with open(rf"{file_name}", 'a+') as inst:     # opening file(Baseboard.sv) & writing inst            
-                        file = module.read()
-                        if "input" and "output" in file:
-                            R_end = (inst.tell())-11
-                            inst.truncate(R_end)                                #remove endmodule 
-                            inst.write(f"\n{inst_name} inst\n(\n")              # calling module (name) & writing it to file
-                            os.chdir('..')
-                            os.chdir('library')
-                            with open(f"{module_name}",'r+') as module2:           # opening module(counter etc)
-                                #print(module2.read())# ------------------
-                                while find:
-                                    find  = module2.readline()
-                                    #print(find) #->>>>>>>>>>>>>>>>>>>>>>>>.
-                                    if 'input' in find:
-                                        inputs = find.replace(",","").split()                #removing ',' & making list
-                                        #print(inputs)#->>>>>>>>>>>>>>>>>.....
-                                        #print("inputs are :",inputs[-1])
-                                        inst.write((f".{inputs[-1]} \t\t\t\t\t (   ),\n")) 
-                                        inp = True
-                                        # input_list[f'input {no_of_inputs}'] = inputs[-1]
-                                        input_list.append(f'{inputs[-1]}')
-                                        #print(input_list)
-                                        no_of_inputs+=1
-                                        # print("no of inputs are :",no_of_inputs)
-                                    elif 'output' in find:
-                                        outputs = find.replace(",","").split()
-                                        #print("outputs are: ",outputs[-1])
-                                        inst.write((f".{outputs[-1]} \t\t\t\t\t (   ),\n"))
-                                        # output_list[f'output {no_of_outputs}'] = outputs[-1]
-                                        output_list.append(f'{outputs[-1]}')
-                                        out = True
-                                        no_of_outputs+=1
-                                       # print("no of outputs are :",no_of_outputs)
-                            if inp and out:      
-                                print("inputs found ")
-                                print("Outputs found ")
-                                print("instance created!\n")
-                                inst.write("\n);" "\n\nendmodule\n")
-                                module.close()
-                                module2.close()
-                                inst.close()
-                                
-                                newline = f'\n{inst_name} = '
-                                module_list = [no_of_inputs,no_of_outputs,input_list,output_list]
-                                data = str(module_list)
-                                print(data)
-                                os.chdir('..')
-                                os.chdir(folder_name)
-                                with open('key_val.py','a+') as key_val_file:
-                                    key_val_file.write(newline)
-                                    key_val_file.write(data)
-                                input_list.clear()
-                                output_list.clear()
-                                no_of_inputs = 0
-                                no_of_outputs = 0
-                                module_list.clear()
-                                os.chdir('..')
-                                
-                        elif inp!=True or out!=True:
-                            print("\ninputs are not found!")
-                            print("outputs are not found!")
-                            print("instance is not created!\n")
-            break               
-        else:
-            print(f"module {module_name} not found!\n")
-            break
+import argparse
+import json
+import Extracting_data
 
-    
+os.chdir('..\Baseboard')
+with open("key_val_file.json", "r")as f:
+    content = json.load(f)
+    for i in content:
+        fileName = content['toplevelfile']['file_name']
+        folder_name = content['toplevelfile']['folder_name']
+        child_path = content['toplevelfile']['child_path']
+
+
+os.chdir('..\library')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--instance_name',
+                    default='inst_uut', help='Name of instance')
+parser.add_argument('-f', '--file_name',
+                    help='Name of file from which instance is taken', required=True)
+args = parser.parse_args()
+file = args.file_name
+instance = args.instance_name
+
+
+def extract_data(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    module_name = "module_name"
+    in_module = False
+    input_or_output_count = 0
+    output_string = ""
+    for line in lines:
+        if 'module' in line and not in_module:
+            in_module = True
+            module_name = line.split()[1]
+            output_string += module_name + ' ' + f'{instance}' + '\n'
+            output_string += "(\n"
+        if 'input' in line or 'output' in line:
+            input_or_output_count += 1
+            words = line.strip().split()
+            x = words[-1]
+            if "," in x:
+                x = x.split(",")[0]
+            if input_or_output_count == sum(('input' in line) or ('output' in line) for line in lines):
+                output_string += '.' + x + '\t\t\t()\n'
+            else:
+                output_string += '.' + x + '\t\t\t(),\n'
+    os.chdir('..')
+    os.chdir('Baseboard')
+    with open(f"{fileName}", "r") as f:
+        content = f.read()
+        lines = content.split('\n')
+        for i, line in enumerate(lines):
+            if instance in line:
+                print(
+                    f'instance {instance} already exists at line {i+1}. Please Enter another name ')
+                exit()
+        with open(f"{fileName}", "a+") as f:
+            if 'endmodule' in content:
+                r_end = (f.tell())-9
+                x = f.truncate(r_end)
+                f.write('\n\n' + output_string)
+                f.write(');')
+                f.write('\n\nendmodule')
+
+
+extract_data(file)
+
+os.chdir('..')
+os.chdir('library')
+data = Extracting_data.get_ranges_from_file(file)
+
+os.chdir('..')
+os.chdir('Baseboard')
+with open("key_val_file.json", "r") as f:
+    content = f.read()
+with open('key_val_file.json', 'a+') as f:
+    if "}" in content:
+        r_end = (f.tell())-3
+        x = f.truncate(r_end)
+    f.write(f',\n\"{instance}\":')
+    json.dump(data, f, indent=4)
+    f.write("\n}")
