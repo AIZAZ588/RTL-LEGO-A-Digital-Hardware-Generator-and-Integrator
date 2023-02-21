b0897684e36271f7eb31bb73adfd1a161cf5efde
Aizaz <108629295+AIZAZ588@users.noreply.github.com>
2023-02-21 11:47:12 +0500

    all arguments are now passed to .py

--- connect.sh
+++ connect.sh
@@ -12,140 +12,22 @@ GREEN=$'\e[1;32m'
 
 #===============Checking file =========================
 
-#if [[ ! -x $PATH ]]
-#then
-#	chmod +x $PATH;           #--> Error: chmod command not found!
-#
-#elif [[ ! -e $PATH ]]
-#then
-#	echo "${PATH} NOT FOUND!";
-#	exit 1
-#fi
-
 #======================================================
 
 usage(){
 
-#echo "$(basename $0) :USAGE"      # --> Error :basename command not found ??
+#echo "$(basename $0) :USAGE"
 echo $WHITE "${0} :UASGE";
 echo $YELLOW	       "$0 -i | --input_inst [name] -ip | --input_ports  [name]";
 echo "	            -o | --output_inst [name] -op | --output_ports [name] ";
+	exit
 
 }
-#if [[ $(${#} + 2) != 0 ]]
-#then
-#	echo "last argument has no value!"
-#	usage				    # $(expr $# % 2) -> is not wroking : command not found
-#	exit 1
-#fi
-
-check(){
-
-#echo "input inst    : ${i_inst}";
-#echo "input port    : ${i_port}";
-#echo "output inst   : ${o_inst}";
-#echo "output port   : ${o_port}";
-
-if [[ -z ${i_port} ]]
-then
-	echo $RED "Error:No input ports! ";
-	usage
-	exit 1
-fi
-
-if [[ -z ${i_inst} ]]
-then
-	echo $RED "Error:No input instance!;"
-	usage
-	exit 1
-
-fi
-
-if [[ -z ${o_port} ]]
-then
-	echo $RED "Error:No output ports! ";
-	usage
-	exit 1
-fi
-
-if [[ -z $o_inst ]]
-then
-	echo $RED "Error:No output instance! ";
-	usage
-	exit 1
-
-fi
-}
-
 run(){
-
-check		 #check for missing argumments
-
-#if [[ -n ${i_inst} && -n ${i_port} && -n ${o_inst} && -n ${o_port} ]]
-#then
-${PATH} '-i' ${i_inst} '-ip' ${i_port} '-o' ${o_inst} '-op' ${o_port}
-	#exit 0
-#fi
+	${PATH} $@
 
 }
-
-if [[ $# -gt 1  ]]
-then
-	while [[ $# -gt 1 ]]
-	do
-		case ${1} in
-		-i | --input_inst)     if [[ ${2} = -* || -z ${2} ]]
-				       then
-						echo $RED "Error:input instance not found!";
-						usage
-						exit 1
-					else
-						i_inst=${2}
-					fi
-					;;
-
-		-ip | --input_ports)
-					if [[ ${2} = -* || -z ${2} ]]
-					then
-					   	 echo $RED "Error:input port not found!";
-						 usage
-						 exit 1
-			       		 else
-      					 	 i_port=${2}
-					fi
-					;;
-
-		-o | --output_inst)     if [[ ${2} = -* || -z  ${2} ]]
-				        then
-						echo $RED "Error:output instance not found!";
-						usage
-						exit 1
-				        else
-				  		o_inst=${2}
-				        fi
-				        ;;
-
-		-op | --output_ports)
-					if [[ ${2} = -* || -z ${2} ]] #Reminder! -> allow value in only [a:b] formate
-					then
-						echo $RED "Error:output ports not found!";
-						usage
-						exit 1
-					else
-						o_port=${2}
-					fi
-					;;
-
-		*)  echo $RED "Error : Unknown argumment!";
-		    usage
-		    exit 1
-		;;
-		esac
-
-	shift 2
-	done
-run
-elif [[ $# -eq 1 && $1 = -h || $1 = --help ]]
+if [[ $# -eq 1 && $1 = -h || $1 = --help ]]
 then
 	echo $GREEN " --help";
 	usage
@@ -157,10 +39,9 @@ then
 	usage
 	exit 1
 else
-	echo $RED "Error:No argumments Entered!"
-	exit 1
+	run $@
 fi
-
+#=========================================================================================
 cd ${CURRENT_DIR}
 if [[ -f *.sv ]]
 then
