714ccf9cf28fb0778766e130eda240a8fa7ffafa
Aizaz <108629295+AIZAZ588@users.noreply.github.com>
2023-02-28 16:28:08 +0500

    copying .sv files removed

--- create.sh
+++ create.sh
@@ -5,7 +5,7 @@
 CURRENT_DIR=$(pwd)
 . ~/.LAGO_USR_INFO
 PATH="${LAGO_DIR}/files/create.py"
-cd ${LAGO_DIR}/files
+#cd ${LAGO_DIR}/files
 RED=$'\e[1;31m'
 YELLOW=$'\e[1;33m'
 WHITE=$'\e[1;37m'
@@ -44,24 +44,4 @@ then
 	exit 1
 else
 	run $@
-fi
-
-
-cd ${CURRENT_DIR}
-if [[ -f *.sv ]]
-then
-
-	/bin/rm -r *.sv
-	/bin/cp -r ${LAGO_DIR}/files/Baseboard/*.sv ${CURRENT_DIR}/
-	#/bin/cat *.sv
-	exit 0
-elif [[ ! -f *.sv ]]
-then
-	/bin/cp -r ${LAGO_DIR}/files/Baseboard/*.sv ${CURRENT_DIR}/
-	#/bin/cat *.sv
-	exit 0
-else
-	echo "error in copying file:"
-	echo "please look your file in : ${LAGO_DIR}/files/Baseboard"
-	exit 1
 fi
