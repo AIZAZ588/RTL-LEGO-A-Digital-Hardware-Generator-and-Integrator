cb23426b3ec452cba94b013a32bc97331894af7a
Aizaz <108629295+AIZAZ588@users.noreply.github.com>
2023-03-14 15:55:16 +0500

    Delete clock.sh

--- clock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-create "-f clock.sv" "-i clk reset en count_max_hrs clr_sec count_sec" "-ir None None None [5:0] None [5:0]" "-o clr_min count_min clr_hrs count_hrs" "-or None [5:0] None [5:0]";
-plug "-f up_counter.sv"" -n Sec";
-plug "-f up_counter.sv"" -n Min";
-plug "-f up_counter.sv"" -n Hr";
-
-#connect "-i Sec -ip clk en count_max clr count"
