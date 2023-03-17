5f0db2bfa84d3cce0a537412b27d8662d9d154f7
AIZAZ588 <aizazkhan022017@gmail.com>
2023-03-17 19:51:00 +0500

    updated

--- files/addparam.py
+++ files/addparam.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import re
 import os
 from colorama import Fore
 
@@ -29,60 +28,3 @@ def adding_parameters(filename, param, ranges):
         print(data)
         with open (filename,'w') as topfile:
             topfile.writelines(data)
-
-            #topfile.writelines(contents)
-        #data = data.insert(2,"hello")
-        #print(data)
-    # print("top file is  : ", filename )
-    # # open the file and read its contents
-    # with open(filename, 'r') as file:
-    #     content = file.read()
-    #     #print(content)
-    #     pattern = r'module\s+clock\s*#\(\s*([^)]*)\s*\)'
-    #     match = re.search(pattern, content, re.DOTALL)
-    #     if match:
-    #         #print(match)
-    #         existing_param = match.group(1)
-    #         existing_param = existing_param.rstrip()
-    #         if existing_param != '':
-    #             existing_param += ','
-    #         else:
-    #             existing_param += ' '
-    #         Body = ''
-    #         if param:
-    #             p = ''
-    #             for prm, rng in zip(param, ranges):
-    #                 if prm in existing_param:
-    #                     print(
-    #                         Fore.RED + f"{prm} already exists in {filename}" + Fore.RESET)
-    #                     exit()
-    #                 else:
-    #                     if rng == 'None':
-    #                         param_text = f'\n\tparameter\t{(p.join(prm))},'
-    #                     else:
-    #                         param_text = f'\n\tparameter\t{rng}\t{(p.join(prm))},'
-    #                     Body += param_text  # append the new parameter text to Body
-    #             print(Fore.GREEN + f"{prm} added to {filename}" + Fore.RESET)
-    #         Body = Body.rstrip(',')
-    #         new_text = f"module clock \n#(\n\t{existing_param}{Body}\n)"
-    #         new_content = content.replace(match.group(0), new_text)
-    #         with open(filename, 'w') as f:
-    #             f.write(new_content)
-    #     else:
-    #         pattern_text = 'module clock\n#(\n)'
-    #         if param:
-    #             p = ''
-    #             Body = ''
-    #             for prm, rng in zip(param, ranges):
-    #                 if rng == 'None':
-    #                     param_text = f'\n\tparameter\t{(p.join(prm))},'
-    #                 else:
-    #                     param_text = f'\n\tparameter\t{rng}\t{(p.join(prm))},'
-    #                 Body += param_text  # append the new parameter text to Body
-    #             print(Fore.GREEN + f"{prm} added to {filename}" + Fore.RESET)
-    #             Body = Body.rstrip(',')
-    #             pattern_text = f"module clock\n#(\n\t{Body}\n)"
-    #             # pattern_text += Body
-    #         content = content.replace('module clock', pattern_text)
-    #         with open(filename, 'w') as file:
-    #             file.write(content)
