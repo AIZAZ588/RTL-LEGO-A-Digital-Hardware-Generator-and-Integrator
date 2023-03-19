88d985ff4b5efcb50fa9244ba537b9d04095bff5
AIZAZ588 <aizazkhan022017@gmail.com>
2023-03-19 07:15:56 +0500

    updated

--- files/connect.py
+++ files/connect.py
@@ -32,7 +32,7 @@ def check_range_equality(inst1, inst2, k1, k2):
     instance2 = Top_level_file.replace('.sv','')
     global found
     try:
-        for i1, i2, k1, k2 in zip(args.instance1, instance2,args.input_ports, args.output_ports):
+        for  k1, k2 in zip(args.instance1, instance2,args.input_ports, args.output_ports):
             range1 = data[inst1]['ports'][k1]['range']
             range2 = data[inst2]['ports'][k2]['range']
             if range1 == range2:
@@ -43,7 +43,7 @@ def check_range_equality(inst1, inst2, k1, k2):
                 exit()
         return found
     except KeyError:
-        for i1, i2, k1, k2 in zip(args.instance1,args.input_ports, args.output_ports):
+        for  k1, k2 in zip(args.instance1,args.input_ports, args.output_ports):
             if inst1 not in data:
                 print(
                     Fore.RED + f'Error: Instance {inst1} not found' + Fore.RESET)
@@ -59,7 +59,7 @@ def check_range_equality(inst1, inst2, k1, k2):
             found = False
         return found
 
-def change_line_in_instance(found, instance1, input_ports, output_ports):
+def connect_to_IO(found, instance1, input_ports, output_ports):
     with open(f"{Top_level_file}", 'r') as f:
         content = f.read()
     pattern = rf'{instance1}\s*(([\s\S]*?));'
@@ -96,11 +96,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Change lines in instances in  file')
     # Add arguments for the instances and ports
-    parser.add_argument('-i', '--instance1',help='Name of the first instance')
+    parser.add_argument('-i', '--instance1',type=str,help='Name of the first instance')
+    
+    parser.add_argument('-P', '--parameter',type=str, help='Name of the port')
     
     parser.add_argument('-o', '--instance2',  help='Name of the second instance')
     
-    parser.add_argument('-ip', '--input_ports', nargs='+', type=str,help='Input ports of the first instance')
+    parser.add_argument('-ip', '--input_ports', nargs='+',type=str,help='Input ports of the first instance')
     
     parser.add_argument('-op', '--output_ports', nargs='+', type=str,help='Output ports of the second instance')
     
@@ -116,8 +118,76 @@ if __name__ == '__main__':
     with open(f'{Baseboard_path}/{json_file}', 'r') as f:
        data = json.load(f)
 
-    instance2 = Top_level_file.replace('.sv','')
-    found = check_range_equality(
-       args.instance1, instance2, args.input_ports, args.output_ports)
-    change_line_in_instance(found, args.instance1,
-                           args.input_ports, args.output_ports)
+    
+    if args.instance1 and args.instance2 and args.input_ports and args.output_ports:
+            instance2 = Top_level_file.replace('.sv','')
+            found = check_range_equality(args.instance1, instance2, args.input_ports, args.output_ports)
+            connect_to_IO(found, args.instance1,args.input_ports, args.output_ports)
+            
+        #print(Fore.RED + 'Error: Please provide all the arguments' + Fore.RESET)
+        #print("example: connect -i  <instance1> -o <instance2> -ip <input_port> put_port -op <output_port> -f <top_level_file>")
+        #exit()
+    if args.parameter and args.instance1 and args.input_ports:
+        index = 0
+        index1 = 0
+        if data['parameter'][args.parameter]:
+            args.instance1=str(args.instance1)
+            for port in args.input_ports:
+                if data[args.instance1]['ports'][port]:
+                    with open(f"{Top_level_file}", 'r') as f:
+                        content = f.readlines()
+                        for string in content:
+                            if args.instance1 in string:
+                               index = content.index(string)
+                            if port in string:
+                               index1 = content.index(string)
+                               print(index)
+                               break
+                            if index1 > index:
+                                Modified_port = f".{port} \t\t\t ({args.parameter})\n"
+                                content.pop(index1)
+                                content.insert(index1, Modified_port)
+                                print(Fore.LIGHTGREEN_EX + 'Parameter Connected.' + Fore.RESET)
+                                with open(f"{Top_level_file}", 'w') as f:
+                                    f.writelines(content)
+                    
+                #else:
+                   
+                #print(Fore.RED + f'Error: Port {args.input_ports} not found of instance {args.instance1}' + Fore.RESET)        
+        else:           
+            print(Fore.RED + f'Error: Parameter {args.parameter} not found' + Fore.RESET)
+            choice=input("Do you want to create a new parameter? (y/n)")
+            if choice =='y' or 'yes':
+                param=input("Enter the parameter name: ")
+                val=input("Enter the value: ")
+                LAGO_DIR=''
+                Baseboard_path = os.path.join(LAGO_DIR,'Baseboard')
+                Top_level_file=''
+                from add import LAGO_USR_INFO
+                from addparam import adding_parameters
+                from addparam import parameter_json
+                adding_parameters(Top_level_file,param,val)
+                parameter_json(Top_level_file,param,val,Baseboard_path)
+                args.instance1=str(args.instance1)
+                for port in args.input_ports:
+                    if data[args.instance1]['ports'][port]:
+                            with open(f"{Top_level_file}", 'r') as f:
+                                content = f.readlines()
+                                for string in content:
+                                    if args.instance1 in string:
+                                        index = content.index(string)
+                                    if port in string:
+                                        index1 = content.index(string)
+                                        break
+                                if index1 > index:
+                                    Modified_port = f".{port} \t\t\t ({args.parameter})\n"
+                                    content.pop(index1)
+                                    content.insert(index1, Modified_port)
+                                    print(Fore.LIGHTGREEN_EX + 'Parameter Connected.' + Fore.RESET)
+                                    with open(f"{Top_level_file}", 'w') as f:
+                                        f.writelines(content)
+            else:
+                print("Exiting...")
+                exit()  
+                
+            
